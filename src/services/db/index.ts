b303a1d7130e5a7cffcff59103892f2d57879165
Brian Anggriawan <briananggriawan31@gmail.com>
2026-09-17 10:05:07 +0700

    fix: connection

--- src/services/db/index.ts
+++ src/services/db/index.ts
@@ -1,6 +1,13 @@
-import { PrismaClient } from "@prisma/client/edge";
-import { withAccelerate } from "@prisma/extension-accelerate";
+import { PrismaClient } from "@prisma/client";
 
-export const prisma = new PrismaClient({
+const globalForPrisma = globalThis as unknown as {
+  prisma?: PrismaClient;
+};
+
+export const prisma = globalForPrisma.prisma ?? new PrismaClient({
   log: ["warn", "error"],
-}).$extends(withAccelerate());
+});
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
